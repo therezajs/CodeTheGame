2313df3d4b88e0e97f93ca97d22e2ae9de2da6fa
Thereza <therezascherrer@gmail.com>
2014-04-03 22:40:28 -0700

    New front page

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,24 +1,24 @@
 module GamesHelper
 
   def display_board
-    "#{display_line(0)}" +
-    "- - -\n" +
-    "#{display_line(1)}" +
-    "- - -\n" +
-    "#{display_line(2)}"
+    # "#{display_line(0)}" +
+    # "- - -\n" +
+    # "#{display_line(1)}" +
+    # "- - -\n" +
+    # "#{display_line(2)}"
   end
 
   def display_element(row, column)
-    element = board[row][column]
-    if element
-      element
-    else
-      ' '
-    end
+    # element = board[row][column]
+    # if element
+    #   element
+    # else
+    #   ' '
+    # end
   end
 
   def display_line(row)
-    "#{display_element(row,0)}|#{display_element(row,1)}|#{display_element(row,2)}\n"
+    # "#{display_element(row,0)}|#{display_element(row,1)}|#{display_element(row,2)}\n"
   end
 
   def create_cell_class(row, column)
